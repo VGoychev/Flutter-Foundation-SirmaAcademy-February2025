0de516f04705b2cd962b0ded920e85d1ead9704e
VGoychev <vladimirgoichev@gmail.com>
2025-03-23 11:23:06 +0200

    Initial commit

--- 04. Flutter Tree & Collection/gesture_detector/lib/main.dart
+++ 04. Flutter Tree & Collection/gesture_detector/lib/main.dart
@@ -29,6 +29,7 @@ class _GestureDetectorState extends State<GestureDetectorScreen>{
   String swipeLeftText = "Swiped Left!";
   String swipeRightText = "Swiped Right!";
   String longPressedText = "Long Pressed!";
+  Offset position = Offset(0, 0);
 
   void changeText(){
     setState(() {
@@ -59,77 +60,102 @@ class _GestureDetectorState extends State<GestureDetectorScreen>{
     });
   }
 
+  void updatePosition(DragUpdateDetails details) {
+    setState(() {
+      position += details.delta;
+      print('New position: x=${position.dx}, y=${position.dy}');
+    });
+  }
 
-
-
-@override
+  @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
         title: Text('Gesture Detector'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(10),
-              child: Text(
-                text,
-                style: TextStyle(
-                  fontSize: 18,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.w600
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    text,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w600
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
                   ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.star),
+                      Icon(Icons.favorite),
+                    ],
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => tapText(),
+                  onLongPress: () => longPress(),
+                  onHorizontalDragUpdate:(details) {
+                    if(details.primaryDelta! > 0){
+                      swipeRight();
+                    } else if (details.primaryDelta! < 0){
+                      swipeLeft();
+                    }
+                  },
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  width: 120,
+                  height: 80,
+                  color: Colors.blue,
+                  alignment: Alignment.center,
+                  child: Text(tappedText,
                   textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.yellow,
+                    fontWeight: FontWeight.bold
+                    ),
+                  ),
                 ),
-              ),
-            Container(
-              margin: EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.star),
-                  Icon(Icons.favorite),
-                ],
-              ),
+                ),
+                ElevatedButton(
+                  onPressed: () => changeText(),
+                  child: Text("Press me!")
+                ),
+              ],
             ),
-            GestureDetector(
-              onTap: () => tapText(),
-              onLongPress: () => longPress(),
-              onHorizontalDragUpdate:(details) {
-                if(details.primaryDelta! > 0){
-                  swipeRight();
-                } else if (details.primaryDelta! < 0){
-                  swipeLeft();
-                }
-              },
-            child: Container(
-              margin: EdgeInsets.all(10),
-              width: 120,
-              height: 80,
-              color: Colors.blue,
-              alignment: Alignment.center,
-              child: Text(tappedText,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.yellow,
-                fontWeight: FontWeight.bold
+          ),
+          Positioned(
+            left: position.dx,
+            top: position.dy,
+            child: GestureDetector(
+              onPanUpdate: updatePosition,
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+                child: Center(
+                  child: Text(
+                    'Drag me',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ),
             ),
-            ),
-            ElevatedButton(
-              onPressed: () => changeText(),
-              child: Text("Press me!")
-              ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
-
 }
 
 
